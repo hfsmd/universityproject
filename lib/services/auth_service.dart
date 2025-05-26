f3c1ee0a516350eb752f032d8ce3a05b7d9c5633
madi <handsfromass123@gmail.com>
2025-05-26 17:02:54 +0500

    попытка подключения бэка через сервис

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  static const String baseUrl = 'http://10.0.2.2:8080/api/auth'; // для эмулятора
+
+  static Future<String?> register(String name, String email, String password) async {
+    final url = Uri.parse('$baseUrl/register');
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'name': name, 'email': email, 'password': password}),
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
+}
